8aab8c1b9e2e4fda571bcbb449022e036939397a
Jason Yao <Jason@Jasons-MacBook-Pro.local>
2015-09-16 18:42:07 -0400

    Assignment finished

--- lab0.py
+++ lab0.py
@@ -3,121 +3,95 @@
 __author__ = 'Jason Yao'
 
 # Global Imports
-import csv
 import sys
 
 # Math Plot Imports
 import numpy as np
 from matplotlib import pyplot as plt
-from mpl_toolkits.axes_grid.axislines import SubplotZero
 
-# Returns a column from a csv file
-def getColumn(filename, column):
-    read = csv.reader(open(filename), delimiter=",")
-    csvList = list(read)
-    rows = len(csvList)
+##
+# NOTE: THIS PROGRAM DEPENDS ON STANDARD INPUT: TO RUN, USE THIS COMMAND IN THE TERMINAL:
+# python3 lab0.py input.csv # WHERE input.csv is the filename, or the path to the file if not in the same directory
+#
 
-    columnValues = []
-    for rowValue in range(0, rows):
-        columnValues.append(csvList[rowValue][column])
-    return columnValues
+# Graphs shit
+def graphShit(dataTitle, xLabel, yLabel, xData, yData, tError, uncertainty):
+    # Gets every subplot into the same figure
+    fig = plt.figure(1, figsize=(14,10))
+    plt0 = fig.add_subplot(3,1,1)
+    plt1 = fig.add_subplot(3,2,3)
+    plt2 = fig.add_subplot(3,2,4)
+    plt3 = fig.add_subplot(3,2,5)
+    plt4 = fig.add_subplot(3,2,6)
 
-def convertShit(stringArray):
-    floatArray = []
-    for i in range(1, len(stringArray)):
-        floatArray.append(float(stringArray[i][0]))
-    return floatArray
-
-def graphShit(figure, xLabel, yLabel, xData, yData):
-    fig = plt.figure(1)
-    plt.title(figure,)
-    plt.xlabel(xLabel)
-    plt.ylabel(yLabel)
-
-    # Generates the axes
-    ax = SubplotZero(fig, 111)
-    fig.add_subplot(ax)
-
-    for direction in ["xzero", "yzero"]:
-        ax.axis[direction].set_axisline_style("-|>")
-        ax.axis[direction].set_visible(True)
-
-    for direction in ["left", "right", "bottom", "top"]:
-        ax.axis[direction].set_visible(False)
-
-    # Generates the best fit line & Draws it
+    # Least squares line calculations
     m, b = np.polyfit(xData, yData, 1)
+    mNice = "%.2f" % m
+    bNice = "%.2f" % b
+    linearEquation = "Linear fit: y = " + str(mNice) + "x" + str(bNice)
 
-    x = np.linspace(0, 0, len(xData))
-    filler = []
-    for i in range(0, len(xData)):
-        filler.append(0)
-    ax.plot(x, filler)
-    ax.scatter(xData, yData)
-    ax.plot(xData, m*xData + b, '-')
-    ax.annotate('Least Squares Line:\n' + 'y = ' + str(m) + 'x +' + str(b), xy=(-10, 14), xytext=(-200, 17),
-            arrowprops=dict(facecolor='black', shrink=0.05))
+    # Chi weighted line of best fit calculations
+    xShit = xData/(uncertainty * uncertainty)
+    yShit = 1/(uncertainty * uncertainty)
+    xHat = sum(xShit)/sum(yShit)
 
-    # Annotates the y-intercept
-    yintercept = b
-    ax.annotate('Y-intercept:\n' + "(0, " +  str(b) + ")", xy=(0, 14), xytext=(40, 10),
-            arrowprops=dict(facecolor='black', shrink=0.05))
+    xShit2 = yData/(uncertainty * uncertainty)
+    yShit2 = 1/(uncertainty * uncertainty)
+    yHat = sum(xShit2)/sum(yShit2)
 
+    beta = sum((xData  - xHat) * yData/(uncertainty**2))/sum((xData - xHat) * (xData/(uncertainty**2)))
+    alpha = yHat - beta * xHat
 
-    fig.savefig('graph.pdf', bbox_inches='tight')
+    # Plots data & least squares lines
+    #plt.title("Lab 0: Python Primer")
+    #plt0.xlabel(xLabel)
+    #plt0.ylabel(yLabel)
+    plt0.scatter(xData, yData)
+    plt0.plot(xData, m*xData + b, 'r-', label=linearEquation) # Plots least squares lines
+    plt0.plot(xData, alpha + (beta * xData), label="Chi-squared weighted best fit line")# Plots chi squared line
 
+    # Gets error bars
+    plt0.errorbar(xData, yData, fmt='ro', label="data", yerr=tError, xerr=uncertainty, ecolor='black')
+    plt0.legend(loc = "upper left")
 
-    #
-    #     fig = plt.figure(1)
-    #     ax = SubplotZero(fig, 111)
-    # fig.add_subplot(ax)
-    #
-    # for direction in ["xzero", "yzero"]:
-    #     ax.axis[direction].set_axisline_style("-|>")
-    #     ax.axis[direction].set_visible(True)
-    #
-    # for direction in ["left", "right", "bottom", "top"]:
-    #     ax.axis[direction].set_visible(False)
-    #
-    # x = np.linspace(-0.5, 1., 100)
-    # ax.plot(x, np.sin(x*np.pi))
+    # Angular calculations
+    w_1 = 2 * np.pi * 5             # rad/sec
+    w_2 = 2 * np.pi * 5.2           # rad/sec
+    newA = 0.1                      # Rad
+    t = np.linspace(0, 25, 1000)    # t-step
 
-    plt.show()
+    theta_a = newA * np.cos(w_1 * t) + newA * np.cos(w_2 * t)
+    theta_b = newA * np.cos(w_1 * t) - newA * np.cos(w_2 * t)
 
+    newTheta_a = 2 * newA * np.cos(((w_2 - w_1)/(2))*t) * np.cos(((w_2 + w_1)/(2))*t)
+    newTheta_b = 2 * newA * np.sin(((w_2 - w_1)/(2))*t) * np.sin(((w_2 + w_1)/(2))*t)
 
+    plt1.plot(t, theta_a)
+    plt2.plot(t, newTheta_a)
+    plt3.plot(t, theta_a, t, newTheta_a)
+    plt4.plot(t, theta_b, t, newTheta_b)
+    plt.savefig('graphs.pdf', bbox_inches='tight')
+    plt.show(1) # Shows the graphs
     return
 
 # Reads in data from .csv file
 def main():
     # Grabs the filename from std input
     filename = sys.argv[1]
+    SKIPROW = 5
 
-    with open(filename, newline='') as csvfile: # Automatically closes file-stream IO
-        # Separates out data streams
-        measurementNumber = getColumn(filename, 0)
-        temperatureCelcius = getColumn(filename, 1)
-        pressurePoundsPerInchesSquared = getColumn(filename, 2)
-        estimatedUncertaintyInPressure = getColumn(filename, 3)
-        T_error = getColumn(filename, 4)
+    temperature, pressure, uncertainty, error = np.loadtxt(
+        filename, skiprows=SKIPROW, unpack=True, usecols=(1,2,3,4), delimiter=',')
 
-        # Generates the data arrays
-        newTemp = []
-        newPressure = []
-        for i in range(1, len(temperatureCelcius)):
-            newTemp.append(float(temperatureCelcius[i]))
-        for i in range(1, len(pressurePoundsPerInchesSquared)):
-            newPressure.append(float(pressurePoundsPerInchesSquared[i]))
+    graphPressure = np.array(pressure)
+    graphTemperature = np.array(temperature)
+    graphUncertainty = np.array(uncertainty)
+    graphError = np.array(error)
 
-        newPressure = np.array(newPressure)
-        newTemp = np.array(newTemp)
-
-        # Graphs shit and outputs it to file
-        graphShit("Temperature versus Pressure", "Temperature (c)", "Pressure (lb/in^2)", newTemp, newPressure)
-
-
-
-
-
+    # Graphs shit and outputs it to file
+    graphShit(
+        "Temperature versus Pressure", "Pressure (lb/in^2)", "Temperature (c)",
+        graphPressure, graphTemperature, graphError, graphUncertainty)
     return
 
 # Standard boilerplate to call the main() function to begin the program.
